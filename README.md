7dbf4a42a470e7c4d123ec9ced6a9d0a8c979b26
Brie @ MnA <hello@brie.work>
2019-02-20 19:22:23 -0500

    Half-readme. Uploaded images and examples.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# 19-mna-intern-test
-Test for internship applicants.
+# 19-mna-intern-challenge
+Challenge for internship applicants.
+
+## Instructions
+Review the example video located in `./examples/19-mna-intern-challenge.mov`. In this same directory is `json-variable.js`, an example 
---
+++ examples/json-variable.js
@@ -0,0 +1,28 @@
+const profiles = {
+  'profile': {
+    '1': {
+      'name': 'Pattern One',
+      'img': './images/color1.jpg'
+    },
+    '2': {
+      'name': 'Color Two',
+      'img': './images/color2.jpg'
+    },
+    '3': {
+      'name': 'Color Three',
+      'img': './images/color3.jpg'
+    },
+    '4': {
+      'name': 'Color Four',
+      'img': './images/color4.jpg'
+    },
+    '5': {
+      'name': 'Color Five',
+      'img': './images/color5.jpg'
+    },
+    '6': {
+      'name': 'Color Six',
+      'img': './images/color6.jpg'
+    }
+  }
+};
